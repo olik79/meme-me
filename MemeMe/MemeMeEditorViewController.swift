245b41376645f75cc766ec9bb8baedf3affdcab4
Oliver Körber <Oli@Olivers-MacBook-Pro.fritz.box>
2015-07-04 19:20:29 +0200

    Set constraints in Storyboard, add outlets and action, set type of editor controller to MemeMeEditerViewController

--- MemeMe/MemeMeEditorViewController.swift
+++ MemeMe/MemeMeEditorViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class MemeMeEditorViewController: UIViewController {
+class MemeMeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickImageButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickImageButtonPressed(sender: UIBarButtonItem) {
+        
     }
-
-
 }
 
